1b5e050d9f4f648f1155b0d8ae897bafd4b1ed4f
kothru <1894683+kothru@users.noreply.github.com>
2022-03-25 22:29:38 +0900

    add valid sample

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import RegisterApp from './RegisterApp'
+import ValidApp from './ValidApp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <RegisterApp />
+    <ValidApp />
   </React.StrictMode>,
   document.getElementById('root')
 )
